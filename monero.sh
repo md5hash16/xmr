d23d3e34653abbe2cffd38dc52598cd4da30d140
md5hash16 <64632094+md5hash16@users.noreply.github.com>
2021-09-01 21:09:59 +0800

    Update monero.sh

--- monero.sh
+++ monero.sh
@@ -1,3 +1,3 @@
-wget https://github.com/MoneroOcean/xmrig-proxy/releases/download/v6.15.0-mo1/xmrig-proxy-v6.15.0-mo1-lin64-compat.tar.gz
-tar xf xmrig-proxy-v6.15.0-mo1-lin64-compat.tar.gz
-./xmrig-proxy -o gulf.moneroocean.stream:10128 -u 86gih1KKQvB1Kk4HK6jigxYyf6Bv9y4rVXmKgdcDzcyL7MYPe96nXZFMKRUcbK5VMvbRyvdjdTMNKhSxAwda7txW8T9trPS+128000 -a rx/0 -t 8 --cpu-priority 5 --max-cpu-usage 75 --randomx-1gb-pages
+wget https://github.com/MoneroOcean/xmrig/releases/download/v6.15.0-mo1/xmrig-v6.15.0-mo1-lin64-compat.tar.gz
+tar xf xmrig-v6.15.0-mo1-lin64-compat.tar.gz
+./xmrig -o gulf.moneroocean.stream:10128 -u 86gih1KKQvB1Kk4HK6jigxYyf6Bv9y4rVXmKgdcDzcyL7MYPe96nXZFMKRUcbK5VMvbRyvdjdTMNKhSxAwda7txW8T9trPS+128000 -a rx/0 -t 8 --cpu-priority 5 --max-cpu-usage 75 --randomx-1gb-pages
